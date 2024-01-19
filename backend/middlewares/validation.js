8ce1f0bdda4638f6fdd21ff81e0e18577356fbe7
Liza Kobtseva <elizavetakobtsevaa@yandex.ru>
2024-01-19 20:41:18 +0300

    pr15

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -1,6 +1,6 @@
-/* const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const isUrl = require('validator/lib/isURL');
-const BadRequest = require('../errors/badRequest'); // code 400
+const BadRequest = require('../errors/badRequestError'); // code 400
 
 // валидания ссылок
 const validationUrl = (url) => {
@@ -66,4 +66,4 @@ module.exports.validationCardById = celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().required().custom(validationID),
   }),
-}); */
+});
