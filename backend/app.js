e886311d014ce406eacb204b62fe7e31410e2315
Liza Kobtseva <elizavetakobtsevaa@yandex.ru>
2024-01-19 20:32:02 +0300

    pr15

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,69 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
+const helmet = require('helmet');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const rateLimit = require('express-rate-limit');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const router = require('./routes');
+const auth = require('./middlewares/auth');
+const { MONGO_URL } = require('./config');
+const corsErr = require('./middlewares/corsErr');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const appRouter = require('./routes/index');
-
-const app = express();
 const { PORT = 3000 } = process.env;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
+const { createUser, login } = require('./controllers/users');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
-
-app.use('/', appRouter);
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App listening on port ${PORT}`);
+app.use(requestLogger); // request
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUser);
+app.use(auth);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
 });
+app.use(router);
+app.use(helmet());
+app.use(limiter);
+app.use(errorLogger); // errors
+app.use(corsErr); // cors
+async function connect() {
+  try {
+    await mongoose.set('strictQuery', false);
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+    });
+    console.log(`App connected ${MONGO_URL}`);
+    await app.listen(PORT);
+    console.log(`App listening on port ${PORT}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+app.use(errors());
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+  next();
+});
+// подключаем роуты
+connect();
